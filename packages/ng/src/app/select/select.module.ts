9e388d13e1682dc726c5ee8227c83d03ed5a299d
Jean-Francois Garreau <jean.francois.garreau@gmail.com>
2018-02-16 17:12:41 +0100

    Refactor and add feeder select

--- packages/ng/src/app/select/select.module.ts
+++ packages/ng/src/app/select/select.module.ts
@@ -3,10 +3,10 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { LuSelectPicker } from './picker';
-import { LuSelectOption } from './option';
+import { LuSelectOption, LuOptionFeederDirective, AbstractSelectOptionFeederComponent } from './option';
 import { LuSelectClearerComponent, LuSelectClearerFirstOrDefaultComponent } from './clearer';
-import { LuSelectSearcherComponent} from './searcher';
 import { LuSelectDirective } from './directive';
+import { LuSelectSearcherComponent} from './searcher';
 import { LuSelect } from './select.component';
 import { LuEmptyModule } from '../empty/empty.module';
 import { LuPopoverModule } from '../popover/popover.module';
@@ -22,6 +22,7 @@ import { LuPopoverModule } from '../popover/popover.module';
 	declarations: [
 		LuSelectPicker,
 		LuSelectOption,
+		LuOptionFeederDirective,
 		LuSelectDirective,
 		LuSelectClearerComponent,
 		LuSelectClearerFirstOrDefaultComponent,
@@ -31,6 +32,7 @@ import { LuPopoverModule } from '../popover/popover.module';
 	exports: [
 		LuSelectPicker,
 		LuSelectOption,
+		LuOptionFeederDirective,
 		LuSelectDirective,
 		LuSelectClearerComponent,
 		LuSelectClearerFirstOrDefaultComponent,
